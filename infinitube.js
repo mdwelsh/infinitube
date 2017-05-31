f0ac0c321fe80ed352ca7ba03f53645558607211
Matt Welsh <mdw@mdw.la>
2017-05-30 22:38:48 -0700

    Working on infinite scrolling

--- infinitube.js
+++ infinitube.js
@@ -1,8 +1,8 @@
-var worldWidth = 40;
-var worldHeight = 40;
+var screenWidth = 40;
+var screenHeight = 20;
 var tileSize = 32;
 
-var game = new Phaser.Game(worldWidth * tileSize, worldHeight * tileSize,
+var game = new Phaser.Game(screenWidth * tileSize, screenHeight * tileSize,
     Phaser.AUTO, '', 
     { preload: preload, create: create, update: update, render: render });
 
@@ -23,15 +23,17 @@ var PLATFORM_LEFT = 'castleHalfLeft.png';
 var PLATFORM_CENTER = 'castleHalfMid.png';
 var PLATFORM_RIGHT = 'castleHalfRight.png';
 
-function makeSlice(y) {
+function makeWalls(y) {
   var c;
   for (x = 0; x < 10; x++) {
+    // XXX MDW - Should use game.add.tileSprite here I think
     c = walls.create(x * tileSize, y * tileSize, 'platformer', WALL);
     c.width = tileSize;
     c.height = tileSize;
     c.body.immovable = true;
   }
-  for (x = worldWidth - 10; x < worldWidth; x++) {
+  for (x = screenWidth - 10; x < screenWidth; x++) {
+    // XXX MDW - Should use game.add.tileSprite here I think
     c = walls.create(x * tileSize, y * tileSize, 'platformer', WALL);
     c.width = tileSize;
     c.height = tileSize;
@@ -69,39 +71,45 @@ function makePlatform(x, y, width, onLeft) {
   }
 }
 
+function buildWorld() {
+
+  for (y = 0; y < screenHeight * 2; y++) {
+    makeWalls(y);
+
+    // Make random platforms.
+    if (game.rnd.frac() < 0.2) {
+      var width = game.rnd.integerInRange(3, 8);
+      if (game.rnd.frac() < 0.5) {
+        makePlatform(10, y, width, true);
+      } else {
+        makePlatform(screenWidth - (10 + width), y, width, false);
+      }
+    }
+  }
+}
+
 function create() {
     //  We're going to be using physics, so enable the Arcade Physics system
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
-    //game.add.tileSprite(0, 0, 1920, 1920, 'background');
-    game.world.setBounds(0, 0, 1920, 1920);
+    // Make the world bigger than the game view.
+    game.world.setBounds(0, 0, game.width, game.height * 2);
+
+    game.stage.backgroundColor = '202020';
 
     // The player and its settings
-    player = game.add.sprite((worldWidth / 2) * tileSize, 30, 'player');
+    player = game.add.sprite((screenWidth / 2) * tileSize, 30, 'player');
     player.animations.add('walk', [0, 1, 2, 3, 4, 5], 10, true);
     player.anchor.setTo(.5,.5);
 
     walls = game.add.group();
     walls.enableBody = true;
-    for (y = 0; y < worldHeight; y++) {
-      makeSlice(y);
-    }
-
     platforms = game.add.group();
     platforms.enableBody = true;
     spikes = game.add.group();
     spikes.enableBody = true;
 
-    // Make some random platforms.
-    for (i = 0; i < 10; i++) {
-      var yval = game.rnd.integerInRange(0, worldHeight);
-      var width = game.rnd.integerInRange(1, 8);
-      if (game.rnd.frac() < 0.5) {
-        makePlatform(10, yval, width, true);
-      } else {
-        makePlatform(worldWidth - (10 + width), yval, width, false);
-      }
-    }
+    buildWorld();
 
     //  We need to enable physics on the player
     game.physics.arcade.enable(player);
@@ -118,38 +126,58 @@ function create() {
 
 }
 
+var wrapping = true;
+
 function update() {
 
     var hitWalls = game.physics.arcade.collide(player, walls);
     var hitPlatform = game.physics.arcade.collide(player, platforms);
+    var hitSpikes = game.physics.arcade.overlap(player, spikes);
 
-    player.body.velocity.x = 0;
+    // XXX MDW - This approach doesn't really do what I want.
+    // I think I want to follow this pattern instead:
+    // https://www.joshmorony.com/how-to-create-an-infinite-climbing-game-in-phaser/
+    // which pre-creates a bunch of tiles and then repositions them as needed.
+
+    // See if we have wrapped.
+    if (!wrapping && player.y < game.height) {
+      wrapping = true;
+      console.log('MDW: Wrapping!');
+//      walls.destroy();
+//      platforms.destroy();
+//      spikes.destroy();
+      buildWorld();
+    } else if (player.y >= game.height) {
+      wrapping = false;
+    }
 
     if (cursors.left.isDown) {
-        //  Move to the left
-        player.body.velocity.x = -150;
+      //  Move to the left
+      player.body.velocity.x = -150;
 
-        // Flip to point left
-        player.scale.x = -1;
-        player.animations.play('walk');
+      // Flip to point left
+      player.scale.x = -1;
+      player.animations.play('walk');
     } else if (cursors.right.isDown) {
-        //  Move to the right
-        player.body.velocity.x = 150;
+      //  Move to the right
+      player.body.velocity.x = 150;
 
-        // Flip to point right
-        player.scale.x = 1;
-        player.animations.play('walk');
+      // Flip to point right
+      player.scale.x = 1;
+      player.animations.play('walk');
     } else if (cursors.down.isDown) {
-        // Move down
-        player.body.velocity.y = 150;
+      // Move down
+      player.body.velocity.y = 150;
     } else if (cursors.up.isDown) {
-        // Move down
-        player.body.velocity.y = -150;
+      // Move down
+      player.body.velocity.y = -150;
     } else {
-        //  Stand still
-        player.animations.stop();
-        player.frame = 4;
+      // Stand still
+      player.animations.stop();
+      player.frame = 4;
     }
+
+    game.world.wrap(player, -(game.height/2), false, false, true);
 }
 
 function render() {
